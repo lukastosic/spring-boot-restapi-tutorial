32f2714aa08ed4f3c5aa3450bfd7220762cc146f
Luka Stosic <lstosic@infodation.nl>
2017-04-14 12:43:39 +0200

    init commit

---
+++ src/main/java/nl/infodation/SpringtutorialApplication.java
@@ -0,0 +1,12 @@
+package nl.infodation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtutorialApplication.class, args);
+	}
+}
